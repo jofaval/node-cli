f8839cec36f762b7cae181e61c3b71998c889ae6
pepe_fabra_valverde <jofaval@outlook.com>
2022-12-14 23:45:34 +0100

    feat(templates): Create example Vue component

---
+++ core/templates/components/vue-kebab-case/PascalCase.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="kebab-case__container">
+    <div class="kebab-case">
+      <slot />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+export default {
+  data() {
+    return {
+      foo: "bar",
+    };
+  },
+  components: {},
+};
+</script>
+
+<style lang="scss" scoped>
+.kebab-case {
+  &__container {
+  }
+}
+</style>
