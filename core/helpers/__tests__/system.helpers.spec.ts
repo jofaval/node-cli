9ae8facbffa42e9e3cd2f17c54278ebbf1609ef4
pepe_fabra_valverde <jofaval@outlook.com>
2022-12-23 18:19:36 +0100

    tests(system): Add base logic exploration

--- core/helpers/__tests__/system.helpers.spec.ts
+++ core/helpers/__tests__/system.helpers.spec.ts
@@ -1,10 +1,34 @@
 // Vendors
+import path from "path";
 import { describe, it, expect } from "vitest";
 // Helpers
 import { getAllFiles, joinPaths } from "../system.helpers";
 
 describe("System utilities", () => {
-  it("should", () => {
-    expect(false).toBe(true);
+  it("should join paths", () => {
+    expect(joinPaths("hello", "world", "foo", "bar")).toBe(
+      "hello/world/foo/bar"
+    );
+  });
+
+  it("should get the file structure", () => {
+    const mocksBasename = path.join(
+      path.dirname(path.dirname(__dirname)),
+      "__mocks__"
+    );
+
+    const filePaths = getAllFiles(
+      path.join(__dirname, "../__mocks__/system")
+    ).map((file) => file.replace(mocksBasename, ""));
+
+    expect(filePaths).toEqual(
+      expect.arrayContaining([
+        "example/style.css",
+        "example/sub-nested/index.ts",
+        "main.scss",
+        "nested/root.tsx",
+        "root.ts",
+      ])
+    );
   });
 });
